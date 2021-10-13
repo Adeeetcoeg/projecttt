0cd29bb99020152cc548eb054b237302a7f96d31
Adeeetcoeg <coegpengencuan@gmail.com>
2021-10-13 08:07:10 +0700

    maap telat push hehe

---
+++ gaji/content/home.php
@@ -0,0 +1,7 @@
+<h1>Penggajian PT SRIKANDI MITSUBISHI MOTORS </h1>
+<hr>
+
+        <div class="col-md-6">
+            <img class="img img-responsive" src="img/company.jpg" />
+        </div>
+<h1>Kami akan Melayani 24 jam </h1>
---
+++ gaji/login.php
@@ -0,0 +1,54 @@
+<?php
+include "conect/crud.php";
+include "proses/p_login.php";
+if (isset($_SESSION['id'])) {
+	echo "<script>document.location='index.php'</script>";
+} else {
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login With Your account</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="bg-light">
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+
+        <h4>Masuk ke PT SRIKANDI</h4>
+        <p>Belum punya akun? <a href="register.php">Daftar di sini</a></p>
+
+        <form action="" method="POST">
+
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input class="form-control" type="text" name="username" placeholder="Username atau email" />
+            </div>
+
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input class="form-control" type="password" name="password" placeholder="Password" />
+            </div>
+
+            <input type="submit" class="btn btn-success btn-block" name="login" value="Masuk" />
+
+        </form>
+            
+        </div>
+
+        <div class="col-md-6">
+            <img class="img img-responsive" src="img/company.jpg" />
+        </div>
+    </div>
+</div>
+    
+</body>
+</html>
